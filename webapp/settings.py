0548a24cb76b0ddc7d4e5480816be9bc922b024c
npi2cash <86048656+npi2cash@users.noreply.github.com>
2021-06-22 16:31:26 +0530

    Update settings.py

--- webapp/settings.py
+++ webapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-d1zfy(yi3kp!pugmwj^c9cjmb+cx6ny!ewcufuci05ddgi=o*f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
